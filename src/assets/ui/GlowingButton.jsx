2ad92420f5b63cb32cab77f701279f58f61676e6
xScafu <a.scarlata93@gmail.com>
2025-06-20 00:55:57 +0200

    first commit

--- src/assets/ui/GlowingButton.jsx
+++
@@ -1,9 +0,0 @@
-import "./glowingButton.css";
-
-export default function GlowingButton() {
-  return (
-    <button className="glowingButton">
-      <p className="buttonText">Hover and click for magic</p>
-    </button>
-  );
-}
